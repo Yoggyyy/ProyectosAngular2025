88e075db79115aef1a63ca94678be601c3ea5b68
«Yoggyyy» <jsantorupv@gmail.com>
2025-12-18 17:10:24 +0100

    Practica semana 1 acabada

---
+++ anguar-events/src/app/events-show/events-show.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IEvents } from '../interfaces/i-events';
+
+@Component({
+  selector: 'events-show',
+  imports: [],
+  templateUrl: './events-show.html',
+  styleUrl: './events-show.css',
+})
+export class EventsShow {
+
+
+
+  events : IEvents [] = [
+    {
+      title: 'Evento de Pueba',
+      imageUrl: '/ssd.jpg',
+      date: new Date('2016-10-03'),
+      description: 'Tremendo partido',
+      price: 23.95,
+    },
+    {
+      title: 'Evento de Pueba 2',
+      imageUrl: '/ssd.jpg',
+      date: new Date('2016-10-03'),
+      description: 'Tremendo partido 2 ahora más epico',
+      price: 25.95,
+    }
+  ];
+}
---
+++ anguar-events/src/app/interfaces/i-events.ts
@@ -0,0 +1,7 @@
+export interface IEvents {
+  title: string;
+  imageUrl: string;
+  date: Date;
+  description: string;
+  price: number;
+}
